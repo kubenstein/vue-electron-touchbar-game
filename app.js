e2b1b5109d6f1dfb0ebc3f5ac990abcb42f40a99
Jakub Niewczas <jniewczas@degica.com>
2021-02-26 10:22:07 +0900

    copy dirty poc code

---
+++ app.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow, TouchBar, nativeImage, ipcMain } = require('electron')
+
+const {  TouchBarButton } = TouchBar
+
+let b;
+const touchBar = new TouchBar({
+  items: [
+    b = new TouchBarButton({
+      label: 'loading...',
+      click: console.log
+    }),
+  ]
+})
+
+let window
+
+app.whenReady().then(() => {
+  window = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+  },
+   width: 1138,
+    height: 500,
+  })
+  window.webContents.openDevTools();
+  window.loadFile('index.html')
+  window.setTouchBar(touchBar)
+
+  ipcMain.on('newImage', (event, arg) => {
+    b.label = ""
+    b.icon = nativeImage.createFromDataURL(arg)
+  });
+})
